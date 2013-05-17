c4b310fb1d28c7b49b36a28cd16276741576a41c
Jason Tarre <jason.tarre@gmail.com>
2013-05-17 19:07:15 -0400

    pagination in progress, add comment link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+      
   def index
     @links = Link.all
+    Link.order('created_at DESC').page(params[:page]).per_page(params[:per_page])
   end
+  
 end
