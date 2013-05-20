2b15c22b9c33cf3e83f5093efb4ca7d25bacc583
Jason Tarre <jason.tarre@gmail.com>
2013-05-20 09:20:54 -0400

    add * for user feedback on voting feature

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,8 +2,10 @@ class VotesController < ApplicationController
     before_filter :authenticate_user!
   
   def create
-   @votes = current_user.votes.create(params[:vote])
-   redirect_to :back
+  @vote = current_user.votes.where(:link_id => params[:vote][:link_id]).first
+  @vote ||= current_user.votes.create(params[:vote])
+  @vote.update_attributes(:up => params[:vote][:up])
+  redirect_to :back
 end
   
 end
